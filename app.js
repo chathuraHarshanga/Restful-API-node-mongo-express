6f58452a2929bb66cfe2cd8ce87bda5e8b5a5767
Chathura Harshanga <38973288+chathuraHarshanga@users.noreply.github.com>
2022-01-11 22:40:32 +0530

    mongoose db connection

--- app.js
+++ app.js
@@ -1,11 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
+
 //routs
 app.get("/", (req, res) => {
   res.send("home page");
 });
+
+app.get("/Post", (req, res) => {
+  res.send("post page");
+});
+
+//connect to db
+mongoose.connect(
+  process.env.DB_CONNECTION,
+  { maxPoolSize: 50, wtimeoutMS: 2500, useNewUrlParser: true },
+  () => {
+    console.log("Connected to DB");
+  }
+);
 
 app.listen(PORT, () =>
   console.log(`Server Running on port: http://localhost:${PORT}`)
